2336c430325f8b10130c3acabea79057618c54a0
JustinHartman407 <50879948+JustinHartman407@users.noreply.github.com>
2019-09-10 17:12:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DaysOfLeaveISM4300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          try
+            {
+                Console.WriteLine("Hello, how many days of leave do you wish to have off of work?");
+                int days = int.Parse(Console.ReadLine());
+                if (days < 1 && days < 8)
+                {
+                    Console.WriteLine("This request is Denied.");
+                }
+                else
+                {
+                    Console.WriteLine("Approved for days of leave.");
+                }
+                Console.WriteLine("Thank you for entering your information");
+            }
+            catch
+            {
+                Console.WriteLine("Unsuspected Error");
+            }
+
+                  
+            
+        }
+    }
+}
